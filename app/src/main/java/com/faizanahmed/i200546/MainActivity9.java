4e96b253d5912988cdd9047efd4c4585900cca9e
mirmujeeb9 <i200611@nu.edu.pk>
2023-10-22 21:19:35 +0500

    part 5

--- app/src/main/java/com/faizanahmed/i200546/MainActivity9.java
+++ app/src/main/java/com/faizanahmed/i200546/MainActivity9.java
@@ -10,6 +10,8 @@ import android.widget.ImageView;
 
 public class MainActivity9 extends AppCompatActivity {
 
+    //part 6 function below
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
